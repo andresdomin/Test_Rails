412070afc14a20df095ff9d229008d433454932f
Andres Dominguez <feliandres86@gmail.com>
2013-05-17 20:26:13 +0100

    My first commit in test_rais

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test::Application.config.secret_token = 'd7d0cc4d18cc0554ac8d646b5c90d0bed2432114cf28409ed87fcc446e089fd7ec41993e04d7da6ff2c363c774c5b741efda0b8a96ce973beb191c0bfab68a27'
